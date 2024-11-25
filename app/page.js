26d7d428280c449ae1ed6ec7a74f68f272a26c66
Manikandan <manikandanid565@gmail.com>
2024-11-25 06:55:14 +0530

    without style

--- app/page.js
+++ app/page.js
@@ -1,5 +1,16 @@
+import PostCard from "@/components/PostCard";
+import SearchBar from "@/components/SearchBar";
+import SearchView from "@/components/SearchView";
+import getPostMetadata from "@/utils/getPostMetadata";
+
+
 export default function Home() {
+  const postMetadata = getPostMetadata('recipes')
+
   return (
-    <main></main>
+    <main>
+      <SearchView postMetadata={postMetadata} />
+
+    </main>
   );
 }
