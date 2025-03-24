7ec28d5ae6bcbc02eee8d2da208e562db11f8cbf
unknown <manikandanid565@gmail.com>
2025-03-24 05:39:23 +0530

    restart

--- src/routes/archive/[slug]/+page.js
+++
@@ -1,9 +0,0 @@
-export async function load({ params }) {
-  try {
-    const post = await import(`../../../archive/posts/${params.slug}.svx`);
-    return { post: post.metadata, Content: post.default };
-  } catch (error) {
-    return { status: 404, error: new Error("Post not found") };
-  }
-}
- 
--- src/routes/poem/[slug]/+page.js
+++
@@ -1,8 +0,0 @@
-export async function load({ params }) {
-  try {
-    const poem = await import(`../../../archive/poems/${params.slug}.svx`);
-    return { poem: poem.metadata, Content: poem.default };
-  } catch (error) {
-    return { status: 404, error: new Error("Poem not found") };
-  }
-}
