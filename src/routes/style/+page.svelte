b9499f0e6ee6e340492b7306dca141c6b1074135
Manikandan V <manikandanid565@gmail.com>
2025-04-14 12:20:35 +0530

    Added styles

---
+++ src/routes/style/+page.svelte
@@ -0,0 +1,40 @@
+<h1 class="title">Style Page css</h1>
+<h1>Heading one</h1>
+<h2>Heading Two</h2>
+<h3>Heading Three</h3>
+<p>ducimus laboriosam minus exercitationem, voluptate optio non fugiat veniam? Ducimus incidunt</p>
+<button>Button</button>
+<article>
+	Article Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero nulla natus assumenda
+	ducimus laboriosam minus exercitationem, voluptate optio non fugiat veniam? Ducimus incidunt
+	delectus aperiam enim tempora dolor veritatis consequuntur consectetur at, maxime accusamus,
+	eligendi, deleniti facere? Facere sapiente qui laboriosam eius!
+</article>
+<p class="caption">Caption</p>
+<hr />
+<blockquote>
+	Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex, sed deserunt libero pariatur modi
+	doloribus magni nobis aut ullam alias laborum autem qui doloremque at, vitae porro perferendis.
+	Debitis, dolorum.
+</blockquote>
+<li>Hello</li>
+<pre><code
+		>import random
+
+	quotes = [
+		"Code is like humor. When you have to explain it, it’s bad.",
+		"In order to be irreplaceable, one must always be different.",
+		"First, solve the problem. Then, write the code.",
+		"Experience is the name everyone gives to their mistakes.",
+		"Java is to JavaScript what car is to Carpet.",
+		"Knowledge is power, but enthusiasm pulls the switch."
+	]
+	
+	def random_quote():
+		return random.choice(quotes)
+	
+	if __name__ == "__main__":
+		print("💬 Quote of the Day:")
+		print(random_quote())</code
+	></pre>
+<a href="#">Link</a>
